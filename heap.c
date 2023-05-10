d6b7542e106777735fc895af0dc06ef7f535f63d
Replit user <>
2023-05-10 00:38:27 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,7 +34,7 @@ void heap_pop(Heap* pq){
     if (pq->size==0)return;
     //printf("%i\n",pq->heapArray[0]->);
     //pq->heapArray->data[0];
-    return (void)pq->heapArray->data[0];
+    return (void)pq->heapArray->data;
 }
 /*Implemente la función `Heap* createHeap()`. Esta función crea un nuevo dato de tipo Heap inicializando sus variables. Considere que la capacidad incial es de 3 casillas para el arreglo.
    > Recuerde reservar memoria para el Heap y también para el arreglo `heapArray`.
