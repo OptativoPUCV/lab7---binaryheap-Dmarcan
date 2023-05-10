b5fbe6100aa6a784d469a1f33c37d783f527da21
Replit user <>
2023-05-10 00:56:53 +0000

    new try

--- heap.c
+++ heap.c
@@ -21,11 +21,22 @@ void* heap_top(Heap* pq){
     if (pq->size==0)return NULL;
     return pq->heapArray[0].data;
 }
+/*Implemente la función `void heap_push(Heap* pq, void* data, int p)`. Esta función inserta un nuevo dato con prioridad `p` en el montículo. Si no recuerda como insertar datos en el montículo puede ver las [diapositivas](https://docs.google.com/presentation/d/1ZjXWMf6g05WdICqvno_oyRvorjAThABgbRGbEqc7mYU/edit#slide=id.g55ac49ed61_0_167).
 
-
-
+    > Si el arreglo está lleno aumente la capacidad al **doble + 1** usando la función `realloc` así:
+    > `arreglo = realloc(arreglo, nueva_capacidad)`
+*/
 void heap_push(Heap* pq, void* data, int priority){
-
+    if (pq->size==pq->capac)
+    {
+        pq->heapArray=(heapElem*)realloc(pq->heapArray, ((pq->capac)*2)*sizeof(heapElem));
+        if (pq->heapArray==NULL) 
+        {
+            free(pq);
+            return;
+        }
+        pq->capac*=2;
+    }
 }
 
 void heap_pop(Heap* pq){
