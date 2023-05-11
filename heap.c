e5087eb17c8f971cba779f9068ddd55e6451de43
Replit user <>
2023-05-11 00:14:41 +0000

    new try

--- heap.c
+++ heap.c
@@ -22,11 +22,7 @@ void* heap_top(Heap* pq){
     if (pq->size==0)return NULL;
     return pq->heapArray[0].data;
 }
-/*Implemente la función `void heap_push(Heap* pq, void* data, int p)`. Esta función inserta un nuevo dato con prioridad `p` en el montículo. Si no recuerda como insertar datos en el montículo puede ver las [diapositivas](https://docs.google.com/presentation/d/1ZjXWMf6g05WdICqvno_oyRvorjAThABgbRGbEqc7mYU/edit#slide=id.g55ac49ed61_0_167).
 
-    > Si el arreglo está lleno aumente la capacidad al **doble + 1** usando la función `realloc` así:
-    > `arreglo = realloc(arreglo, nueva_capacidad)`
-*/
 void heap_push(Heap* pq, void* data, int priority){
     if (pq->size==pq->capac)
     {
@@ -38,6 +34,7 @@ void heap_push(Heap* pq, void* data, int priority){
         }
         pq->capac=(pq->capac*2)+1;
     }
+    
     bool entro=false;
     pq->heapArray[pq->size].data=data;
     pq->heapArray[pq->size].priority=priority;
@@ -59,31 +56,21 @@ void heap_push(Heap* pq, void* data, int priority){
     }
     pq->size++;
 }
-/*Implemente la función `void heap_pop(Heap* pq)`. Esta función elimina el mayor elemento del montículo (la raíz). Si no recuerda el procedimiento puede ver las [diapositivas](https://docs.google.com/presentation/d/1ZjXWMf6g05WdICqvno_oyRvorjAThABgbRGbEqc7mYU/edit#slide=id.g10c6e3d52b_0_98).*/
-void mostrarArreglo(Heap* pq)
-{
-    printf("[");
-    for (size_t k=0;k<pq->size;k++)
-    {
-        printf(" %i ",pq->heapArray[k].priority);
-    }
-    printf("]\n");
-}
+
 void heap_pop(Heap* pq){
     if (pq->size==0)return;
-    //printf("%i",pq->heapArray[pq->size-1].priority);
     pq->heapArray[0].data=pq->heapArray[pq->size-1].data;
     pq->heapArray[0].priority=pq->heapArray[pq->size-1].priority;
     pq->size--;
-    //printf("%i\n",pq->size);
+    
     size_t k=0;
     size_t ultiK=k;
-    
     while (k<pq->size)
     {
         ultiK=k;
         if (pq->heapArray[k+1].priority<pq->heapArray[k+2].priority)k=(2*k)+2;
         else k=(2*k)+1;
+        
         int aux=pq->heapArray[k].priority;
         pq->heapArray[k].priority=pq->heapArray[ultiK].priority;
         pq->heapArray[ultiK].priority=aux;
@@ -92,12 +79,9 @@ void heap_pop(Heap* pq){
         pq->heapArray[k].data=pq->heapArray[ultiK].data;
         pq->heapArray[ultiK].data=data;
     }
-    //mostrarArreglo(pq);
     return;
 }
-/*Implemente la función `Heap* createHeap()`. Esta función crea un nuevo dato de tipo Heap inicializando sus variables. Considere que la capacidad incial es de 3 casillas para el arreglo.
-   > Recuerde reservar memoria para el Heap y también para el arreglo `heapArray`.
-*/
+
 Heap* createHeap(){
     Heap* nuevoHeap=(Heap*)malloc(sizeof(Heap));
     if (nuevoHeap==NULL)return NULL;
